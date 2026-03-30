fea58a418035d050bf5a1cfe97435ce4f043cd41
Somasundaram Munirathnam <somasundar.m@gmail.com>
2026-03-30 01:57:01 -0500

    Full Mobile-First Responsive Redesign (Hamburger Menu, Stacked Layouts, Responsive Tables)

--- Employee-Management-UI/src/app/app.component.ts
+++ Employee-Management-UI/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Employee-Management-UI';
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen = false;
+  }
 }

9a00109a795886ffcccd8f474e9f21fed0e29b3f
Somasundaram Munirathnam <somasundar.m@gmail.com>
2026-03-30 00:47:55 -0500

    Set production API URL

--- Employee-Management-UI/src/environments/environment.ts
+++ Employee-Management-UI/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://employee-management-service.onrender.com/api' // Note: This is a placeholder! You will update this with your actual Render URL later.
+  apiUrl: 'https://nexus-hr-backend-p77r.onrender.com/api'
 };

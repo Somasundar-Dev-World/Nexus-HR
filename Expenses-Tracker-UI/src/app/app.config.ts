93d5855725d9948319c625e19336164f67537daf
Somasundaram Munirathnam <somasundar.m@gmail.com>
2026-03-30 23:57:05 -0500

    fix: Enable scrollPositionRestoration to ensure author page starts at top

--- Expenses-Tracker-UI/src/app/app.config.ts
+++ Expenses-Tracker-UI/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
